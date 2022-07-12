145082cb04b6afda8f35b36dfbfe331f9b580f01
Nick Whitehill <nWhitehill@users.noreply.github.com>
2022-07-11 23:48:19 -0600

    Initial commit

---
+++ src/test/java/ATest.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ATest {
+
+    @Test
+   void returnsTrueTest() {
+    assertThat(true).isTrue();
+   } 
+}
